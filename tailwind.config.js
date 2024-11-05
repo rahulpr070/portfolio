341c7a093ce080eab50732767e30e8b578f0f850
rahulpr070 <rahulofficial070@gmail.com>
2024-11-05 16:41:00 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['IBM Plex Sans', 'sans-serif'],
+      'serif': ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [],
+}
